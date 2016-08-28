c9ddc5ba22b8ec7c291e0682d28288dbd34284d8
Antonello <infovillasimius@gmail.com>
2016-08-28 15:31:00 +0200

    sistemazione generale nome package

---
+++ src/nowMessage/Contact.java
@@ -0,0 +1,106 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nowMessage;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Mrk
+ */
+public class Contact {
+    
+    private final String email;
+    private final Image photo;
+    private boolean contactBlocked = false; //di default tutti i contatti non sono bloccati
+    private ArrayList<Message> messages;
+
+    /**
+     * Constructor
+     * @param email indica l'email del contatto da creare
+     * @param photo indica la foto del contatto da creare
+     */
+    public Contact(String email, Image photo) {
+        this.email = email;
+        this.photo = photo;
+    }
+    
+    /**
+     * Constructor
+     * @param email indica l'email del contatto da creare
+     * @param photo indica la foto del contatto da creare
+     * @param messages indica la lista dei messaggi relativi al contatto da creare
+     */
+    public Contact(String email, Image photo, ArrayList<Message> messages) {
+        this.email = email;
+        this.photo = photo;
+        this.messages = messages;
+    }
+    
+    //methods
+    
+    /**
+     * Questo metodo permette di conoscere l'email del contatto
+     * @return l'email del contatto
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * Questo metodo permette di conoscere la foto del contatto
+     * @return la foto del contatto
+     */
+    public Image getPhoto() {
+        return photo;
+    }
+
+    /**
+     * Questo metodo permette di sapere se il contatto è bloccato o meno
+     * @return lo stato del contatto
+     */
+    public boolean isContactBlocked() {
+        return contactBlocked;
+    }
+
+    /**
+     * Questo metodo permette di settare lo stato del contatto
+     * @param contactBlocked indica il nuovo stato da impostare al contatto
+     */
+    public void setContactBlocked(boolean contactBlocked) {
+        this.contactBlocked = contactBlocked;
+    }
+
+    /**
+     * Questo metodo permette di conoscere la lista dei messaggi ricevuti/inviati al contatto
+     * @return la lista dei messaggi
+     */
+    public ArrayList<Message> getMessages() {
+        return messages;
+    }
+
+    /**
+     * Questo metodo permette di settare la lista dei messaggi ricevuti/inviati al contatto
+     * @param messages indica la nuova lista messaggi ricevuti/inviati
+     */
+    public void setMessages(ArrayList<Message> messages) {
+        this.messages = messages;
+    }
+    
+    /**
+     * Questo metodo permette di avere una rappresentazione testuale dell'oggetto Contact
+     * @return la stringa associata
+     */
+    @Override
+    public String toString(){
+        return "Email: " + email
+                + "\nFoto: " + photo
+                + "\nStato: " + contactBlocked;
+    }
+    
+    
+    
+}
---
+++ src/nowMessage/MainViewer.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nowMessage;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Mrk
+ */
+public class MainViewer {
+    
+    private Scanner input = new Scanner(System.in);
+    
+    /*metodi Scanner:
+    - nextLine()
+    - nextInt()
+    - nextDouble()
+    */
+    
+    /**
+     * Questo metodo permette di mostrare all'utente il menu principale
+     * @return la selezione dell'utente
+     */
+    public Integer showFunction(){
+        System.out.println("\n\n**NowMessage**");
+
+        System.out.print("\nInserisci:\n"
+                + "1) Per visualizzare la tua rubrica\n"
+                + "2) Per visualizzare i gruppi salvati\n"
+                + "3) Per creare un nuovo messaggio\n"
+                + "0) Per uscire\n"
+                + "Input: ");
+        
+        return input.nextInt();
+    
+    }
+}
